9ce0273f7f44cd6f9103a3790ab1de3c3636f165
malcomcrab <malcomcrabesq@outlook.com>
2025-02-25 11:39:40 +0000

    Adds routing for CRUD methods.

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const dotenv = require('dotenv/config');
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res) => res.send("Hello, world!"));
-
+const cors = require('cors');
 
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`My first Express app - listening on port ${PORT}!`);
+
+app.get('/', (req, res) => {
+    return res.send('Received a GET HTTP method');
+  });
+  
+  app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+  });
+  
+  app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+  });
+  
+  app.delete('/', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
+  });
+
+
+app.listen(process.env.LOCALPORT, () => {
+  console.log(`My first Express app - listening on port ${process.env.LOCALPORT}!`);
 });
