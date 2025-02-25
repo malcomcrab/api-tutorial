f7504d678b28240bb37e5c12480089c2c1882660
malcomcrab <malcomcrabesq@outlook.com>
2025-02-25 11:22:10 +0000

    first commit: Installs express, dotenv and creates app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv/config');
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello, world!"));
+
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
